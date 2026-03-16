9daf6ca5653f832b766a71799c5bbec3f5238ce4
Sibar Al-ani <sibar.alani@gmail.com>
2026-03-16 10:57:45 +0100

    03-todo-modular done

--- 01-unit-and-integration-tests/03-todo-modular/src/main.js
+++ 01-unit-and-integration-tests/03-todo-modular/src/main.js
@@ -1,45 +1,9 @@
 import {addTodo, removeTodo, getTodoCount, resetTodos} from "./todo-model.js"
+import {addTodoElement, updateStats, clearList} from "./todo-view.js"
 
-/**
- * DOM
- */
-let list = document.getElementById("todo-list");
-let stats = document.getElementById("todo-stats");
-
-function addTodoElement(todo, onRemove) {
-  let li = document.createElement("li");
-  li.className = "todo-item";
-  li.dataset.id = todo.id;
-
-  let span = document.createElement("span");
-  span.className = "todo-text";
-  span.textContent = todo.text;
-
-  let btn = document.createElement("button");
-  btn.className = "todo-remove";
-  btn.textContent = "\u00d7";
-  btn.addEventListener("click", () => {
-    onRemove(todo.id)
-    li.remove();
-  });
-
-  li.append(span, btn);
-  list.appendChild(li);
-}
-
-function updateStats(count) {
-  stats.textContent = count + " sysslor";
-}
-
-
-/**
- * 
- * Running the app (Data & DOM)
- */
 let form = document.getElementById("todo-form");
 let input = document.getElementById("todo-input");
 let clearBtn = document.getElementById("clear-btn");
-list = document.getElementById("todo-list");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -59,6 +23,6 @@ form.addEventListener("submit", (e) => {
 
 clearBtn.addEventListener("click", () => {
   resetTodos()
-  list.innerHTML = "";
-  updateStats(getTodoCount());
+  clearList()
+  updateStats(getTodoCount())
 });
