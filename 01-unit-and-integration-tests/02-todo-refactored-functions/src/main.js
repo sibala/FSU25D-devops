c0c8543f05e2f095ea7a452b3e64276183117925
Sibar Al-ani <sibar.alani@gmail.com>
2026-03-16 09:04:31 +0100

    02-todo-refactored-functions part 1

--- 01-unit-and-integration-tests/02-todo-refactored-functions/src/main.js
+++ 01-unit-and-integration-tests/02-todo-refactored-functions/src/main.js
@@ -1,6 +1,27 @@
+
+
+/**
+ * 
+ * Data logic
+ */
 let todos = [];
 let nextId = 1;
 
+
+function addTodo(text) {
+  let trimmedText = text.trim();
+  if (!trimmedText) return;
+
+  let id = nextId++;
+  let todo = { id: id, text: trimmedText, done: false }
+  todos.push(todo);
+  return todo;
+}
+
+
+/**
+ * DOM
+ */
 let form = document.getElementById("todo-form");
 let input = document.getElementById("todo-input");
 let list = document.getElementById("todo-list");
@@ -9,21 +30,24 @@ let clearBtn = document.getElementById("clear-btn");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let text = input.value.trim();
-  if (!text) return;
+  // let text = input.value.trim();
+  // if (!text) return;
 
-  let id = nextId++;
-  todos.push({ id: id, text: text, done: false });
-  console.log(todos);
+  // let id = nextId++;
+  // todos.push({ id: id, text: text, done: false });
+  // console.log(todos);
+
+  let todo = addTodo(input.value)
+  console.log(todo)
+  
   input.value = "";
-
   let li = document.createElement("li");
   li.className = "todo-item";
-  li.dataset.id = id;
+  li.dataset.id = todo.id;
 
   let span = document.createElement("span");
   span.className = "todo-text";
-  span.textContent = text;
+  span.textContent = todo.text;
 
   let btn = document.createElement("button");
   btn.className = "todo-remove";
@@ -38,6 +62,13 @@ form.addEventListener("submit", (e) => {
   list.appendChild(li);
   stats.textContent = todos.length + " sysslor";
 });
+
+
+
+/**
+ * 
+ * Running the app (Data & DOM)
+ */
 
 clearBtn.addEventListener("click", () => {
   todos = [];
